6c2e5ae509a9dfa4bb67e888205bcc77d5c74b1f
ivandiez11 <ivandiezx2005@gmail.com>
2026-01-15 19:47:03 +0100

    redirigir al index.html

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, EmailStr, field_validator, ValidationError
+from typing import Optional, List
+import re
 
-app = FastAPI()
+app = FastAPI(title = "LibroNet")
+
+# Servir archivos estáticos
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Motor de plantillas
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
 
-@app.get("/")
-def hola():
-    return {"message": "Hola"}
+
+@app.get("/", response_class=HTMLResponse)
+def get_index(request: Request):
+    return templates.TemplateResponse(
+        "pages/index.html",
+        {
+            "request": request
+        }
+    )
